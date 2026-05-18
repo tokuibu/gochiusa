6ff45b5b7703f00973039b1f76dde7ae2c07c99d
tokuibu <Tokue.ibuibu@gmail.com>
2026-05-18 22:52:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1505806483198050324/7caQ_Y_5pA-s81DF0NFGnSanoEIeQxkAIigAQctPpgTax2-OG9MAFcaUD1ikkfeJsEDZ"
 
@@ -11,17 +12,38 @@ headers = {
 
 response = requests.get(SEARCH_URL, headers=headers)
 
-if response.status_code == 200:
-
-    message = {
-        "content": f"ごちうさONKYO検索\n{SEARCH_URL}"
-    }
-
-    requests.post(WEBHOOK_URL, json=message)
-
-else:
-
+if response.status_code != 200:
     requests.post(
         WEBHOOK_URL,
         json={"content": "メルカリ取得失敗"}
+    )
+    exit()
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+links = soup.find_all("a")
+
+sent = False
+
+for link in links:
+
+    href = link.get("href")
+
+    if href and "/item/" in href:
+
+        item_url = "https://jp.mercari.com" + href
+
+        message = {
+            "content": f"【新着候補】\n{item_url}"
+        }
+
+        requests.post(WEBHOOK_URL, json=message)
+
+        sent = True
+        break
+
+if not sent:
+    requests.post(
+        WEBHOOK_URL,
+        json={"content": "商品が見つかりませんでした"}
     )
